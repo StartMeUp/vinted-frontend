b6ec1900890c8ce0af6ce452012a6d240a264e46
StartMeUpLille <contact@start-me-up.fr>
2020-11-11 15:49:58 +0100

    routes ok

---
+++ src/containers/Home.js
@@ -0,0 +1,7 @@
+import React, { useEffect } from "react";
+
+const Home = () => {
+  return <p>homepage</p>;
+};
+
+export default Home;
---
+++ src/containers/Offer.js
@@ -0,0 +1,7 @@
+import React, { useEffect } from "react";
+
+const Offer = () => {
+  return <p>Offer page</p>;
+};
+
+export default Offer;
