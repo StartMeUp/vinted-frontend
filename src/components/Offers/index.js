9406f473e98e7de993a0a4be7dce27139d9cc0aa
StartMeUpLille <contact@start-me-up.fr>
2020-11-11 22:36:08 +0100

    Offer.js issue

--- src/components/Offers/index.js
+++ src/components/Offers/index.js
@@ -1,10 +1,62 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
 import "./index.css";
 
 const Offers = () => {
-  return (
+  const [isLoading, setIsLoading] = useState(true);
+  const [offers, setOffers] = useState([]);
+
+  const fetchData = async () => {
+    try {
+      const response = await axios.get(
+        "https://vinted-back-smu.herokuapp.com/offers"
+      );
+      setOffers(response.data.offers);
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+    setIsLoading(false);
+  }, []);
+
+  return isLoading ? (
+    <p>Loading data</p>
+  ) : (
     <section id="content">
-      <div className="container">Offers</div>
+      <div className="container">
+        {offers.map((offer) => {
+          return (
+            <Link to={`offer/${offer._id}`}>
+              <div className="offer-card">
+                <div className="owner">
+                  <img src={offer.owner.account.avatar.secure_url} alt="" />
+                  {offer.owner.account.username}
+                </div>
+                <img
+                  className="offer-image"
+                  src={offer.product_image.secure_url}
+                  alt=""
+                />
+                <div className="info">
+                  <span className="price">{offer.product_price} €</span>
+                  <br />
+                  <span className="size">
+                    {offer.product_details[1].TAILLE}
+                  </span>
+                  <br />
+                  <span className="brand">
+                    {offer.product_details[0].MARQUE}
+                  </span>
+                </div>
+              </div>
+            </Link>
+          );
+        })}
+      </div>
     </section>
   );
 };
--- src/containers/Offer.js
+++ src/containers/Offer.js
@@ -1,7 +1,49 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { useParams } from "react-router-dom";
 
 const Offer = () => {
-  return <p>Offer page</p>;
+  const { id } = useParams();
+  const [isLoading, setIsLoading] = useState(true);
+  const [offer, setOffer] = useState({});
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(
+          `https://vinted-back-smu.herokuapp.com/offer/${id}`
+        );
+        console.log(response.data);
+        setOffer(response.data);
+      } catch (error) {
+        console.log(error.message);
+      }
+    };
+    fetchData();
+    setIsLoading(false);
+  }, [id]);
+
+  return isLoading ? (
+    <p>Loading</p>
+  ) : (
+    <div className="container">
+      {console.log("offer =>", offer, isLoading)}
+      <img src={offer.product_image.secure_url} alt="" />
+      <div className="offer-details">
+        <p>{offer.product_price}</p>
+        <ul>
+          {offer.product_details.map((detail) => {
+            const values = Object.entries(detail);
+            return (
+              <li>
+                {values[0][0]} : {values[0][1]}
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default Offer;
